338c904ef3c62c73a01e13221976ed3875dc5e37
xiepeizuhang <pzxie@erathink.com>
2017-09-21 15:39:45 +0800

    更新

--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -9,7 +9,6 @@
     <div id="test">
         <h1>hello monaca</h1>
         <img src='../public/assets/image/hdpi.png' alt=''>
-        <img src='../public/assets/image/1.png' alt=''>
     </div>
 </template>
 <script>
